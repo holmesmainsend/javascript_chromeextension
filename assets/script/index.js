0b966c7c75e07ca899b9cae9288fe59191e09eaf
Mona Lisa <holmesmainsend@gmail.com>
2022-11-06 11:50:32 -0500

    tested rendering li directly to page through script

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,12 +1,12 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let myLeads = []
+let myLeads = ["www1", "ww2", "ww3"]
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
 })
 
 for(let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
