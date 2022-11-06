97b743bbef7b3d5bc83cd6b2841d389008835226
Mona Lisa <holmesmainsend@gmail.com>
2022-11-06 11:43:30 -0500

    added ul

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,12 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 let myLeads = []
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
 })
+
+for(let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
