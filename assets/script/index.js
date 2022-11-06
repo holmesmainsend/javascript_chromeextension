8b5cf2d4fc4d258902c0b267c1dc9b65c9fbb267
Mona Lisa <holmesmainsend@gmail.com>
2022-11-06 11:31:35 -0500

    added first array

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,6 @@
 let inputBtn = document.getElementById("input-btn")
+let inputEl = document.getElementById("input-el")
+let myLeads = []
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
