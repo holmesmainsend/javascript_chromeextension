8599858548788c4082800013bc8953a65e6f0120
Mona Lisa <holmesmainsend@gmail.com>
2022-11-06 12:19:42 -0500

    fixed renderLeads function

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,12 +1,18 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let myLeads = ["www1", "ww2", "ww3"]
+let myLeads = []
+
+function renderLeads() {
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
-
-for(let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-}
